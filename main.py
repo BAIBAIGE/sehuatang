7e12606885823f0522fb8aeb367def10de83235d
SingleJohn <SingleJohn@singlelovely.cn>
2022-04-06 14:32:39 +0800

    应对反爬

--- main.py
+++ main.py
@@ -28,8 +28,12 @@ def get_plate_info(fid: int, page: int, proxy, date_time):
         "fid": fid,
         "page": page,
     }
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+        "cookie": "UM_distinctid=17de17aee1edd-006cbd7a1c99c7-4303066-1fa400-17de17aee1fd42; CNZZDATA1280511935=1432301821-1640160379-%7C1640853459; cPNj_2132_smile=1D1; cPNj_2132_saltkey=nIFGzacf; cPNj_2132_lastvisit=1648120917; cPNj_2132_atarget=1; cPNj_2132_visitedfid=103D145D39D36D106D151D107D2D38D152; High=fa8362d9f320d9a85b50de1b51093e77; cPNj_2132_lastfp=d021b67b8716f2f2cdf8caba1e7cfc29; cPNj_2132_lastact=1649221864%09forum.php%09forumdisplay; cPNj_2132_st_t=0%7C1649221864%7Cd66d2ced0dca736763e46bd0c8b31a2c; cPNj_2132_forum_lastvisit=D_104_1648712354D_37_1648815821D_152_1648818323D_38_1648818334D_2_1648818354D_107_1648856490D_151_1648856506D_106_1648856664D_36_1648856880D_39_1648856887D_103_1649221864",
+    }
 
-    response = httpx.get(url, params=params, proxies=proxy)
+    response = httpx.get(url, headers=headers, params=params, proxies=proxy)
     # 使用bs4解析
     soup = bs4.BeautifulSoup(response.text, "html.parser")
     all = soup.find_all(id=re.compile("^normalthread_"))
@@ -65,7 +69,11 @@ def get_page(tid, proxy):
     url = "https://{}/forum.php?mod=viewthread&tid={}".format(
         get_config("domain_name"), tid
     )
-    response = httpx.get(url, proxies=proxy)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+        "cookie": "UM_distinctid=17de17aee1edd-006cbd7a1c99c7-4303066-1fa400-17de17aee1fd42; CNZZDATA1280511935=1432301821-1640160379-%7C1640853459; cPNj_2132_smile=1D1; cPNj_2132_saltkey=nIFGzacf; cPNj_2132_lastvisit=1648120917; cPNj_2132_atarget=1; cPNj_2132_visitedfid=103D145D39D36D106D151D107D2D38D152; High=fa8362d9f320d9a85b50de1b51093e77; cPNj_2132_lastfp=d021b67b8716f2f2cdf8caba1e7cfc29; cPNj_2132_lastact=1649221864%09forum.php%09forumdisplay; cPNj_2132_st_t=0%7C1649221864%7Cd66d2ced0dca736763e46bd0c8b31a2c; cPNj_2132_forum_lastvisit=D_104_1648712354D_37_1648815821D_152_1648818323D_38_1648818334D_2_1648818354D_107_1648856490D_151_1648856506D_106_1648856664D_36_1648856880D_39_1648856887D_103_1649221864",
+    }
+    response = httpx.get(url, headers=headers, proxies=proxy)
     soup = bs4.BeautifulSoup(response.text, "html.parser")
     # 获取帖子的标题
     title = soup.find("h1", class_="ts").find("span").get_text()
@@ -127,6 +135,7 @@ def main():
                 continue
         log.info("即将开始爬取的页面 " + " ".join(tid_list_all))
         data_list = []
+        tid_list = []
         for i in info_list_all:
             try:
                 data = get_page(i["tid"], proxy)
@@ -138,13 +147,14 @@ def main():
                 # 再次匹配发布时间（因为上级页面获取的时间可能不准确）
                 if re.match("^" + date_time, post_time):
                     data_list.append(data)
+                    tid_list.append(data["tid"])
             except Exception as e:
                 log.error("Crawl the page " + " ".join(list(i.values())) + " failed.")
                 log.error(e)
                 continue
             finally:
                 continue
-        log.info("本次抓取的数据条数为：" + str(len(data_list)))
+        log.info("本次抓取的数据条数为：" + str(len(data_list)) + " 分别为： " + " ".join(tid_list))
         log.info("开始写入数据库")
         save_data(data_list, fid)
 
